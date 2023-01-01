200810cb0b248cc6392d21635b9ab07845f6f4b1
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2023-01-02 00:15:28 +0530

    Create Q3_equalNotequal.java

---
+++ Java/Operators/Q3_equalNotequal.java
@@ -0,0 +1,20 @@
+// Program to equal operator and not equal operators
+
+class equalNotequal{
+    public static void main(String args[]){
+        int num1 = 1;
+        String num2 = "1";
+        int num3 = 1;
+       
+        if(num1 == num3)
+            System.out.print("Equal");
+            else
+            System.out.print("Not Equal");
+
+         if(num1 != num2)
+            System.out.print("True");
+            else
+            System.out.print("False");
+     
+    }
+}
