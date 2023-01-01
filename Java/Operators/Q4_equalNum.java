e564e52b4367c5cbcff148dc640e254440419d09
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2023-01-02 00:17:24 +0530

    Create Q4_equalNum.java

---
+++ Java/Operators/Q4_equalNum.java
@@ -0,0 +1,12 @@
+//  Write a program to find the two numbers equal or not
+
+class equalNum{
+    public static void main(String args[]){
+        int num0 = 18;
+        int num1 = 10;
+        if(num0 == num1)
+            System.out.print("Equal");
+            else
+            System.out.print("Not Equal");
+    }
+}
