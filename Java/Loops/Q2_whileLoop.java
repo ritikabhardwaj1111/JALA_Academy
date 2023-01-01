18221aab6d38ca3659ce93553e43399f887f8e87
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2023-01-02 00:37:04 +0530

    Create Q2_whileLoop.java

---
+++ Java/Loops/Q2_whileLoop.java
@@ -0,0 +1,12 @@
+// Write a java program to print 1 to 20 numbers using the while loop.
+
+class whileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        while(num != 21)
+        {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
