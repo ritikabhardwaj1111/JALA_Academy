7c7df8fd7ddccd1a00c5f4f820b9a32661675df4
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2023-01-02 00:24:15 +0530

    Create Q5_logicalOperator.java

---
+++ Java/Operators/Q5_logicalOperator.java
@@ -0,0 +1,14 @@
+// Programs on Logical AND,OR operator and Logical NOT
+
+class logicalOperator{
+    public static void main(String args[]){
+       System.out.print((7<8)&&(9>1));
+      
+       System.out.println((12>9 || 13<0));
+      
+       if('a' != 'A')
+            System.out.println("Not equal");
+            else
+            System.out.println("Equal");
+    }
+}
