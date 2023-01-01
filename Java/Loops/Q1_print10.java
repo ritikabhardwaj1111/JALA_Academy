e2baa0a79802dbe2590685c3821e097a317ca76c
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2023-01-02 00:34:48 +0530

    Create Q1_print10.java

---
+++ Java/Loops/Q1_print10.java
@@ -0,0 +1,8 @@
+// Write a program to print “Bright IT Career” ten times using for loop
+
+class print10 {
+    public static void main(String[] args) {
+        for(int i = 0 ; i < 10 ; i++)
+            System.out.println("Bright IT Career");
+    }
+}
