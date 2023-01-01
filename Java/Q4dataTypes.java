f3ff2650673988916ae5cd439170729c9746c939
Ritika Bhardwaj <65389514+ritikabhardwaj1111@users.noreply.github.com>
2023-01-01 23:55:54 +0530

    Create Q4dataTypes.java

---
+++ Java/Q4dataTypes.java
@@ -0,0 +1,14 @@
+//Define variables for different Data Types int, Boolean, char, float, double and print on the Console
+
+class dataTypes{
+    public static void main(String args[]){
+        int a = 8;
+        float b = 1.9f;
+        Boolean c = true;
+        double d = 9.999999;
+        System.out.println("Integer: "+a);
+        System.out.println("Float: "+b);
+        System.out.println("Boolean: "+c);
+        System.out.println("Double: "+d);
+    }
+}
